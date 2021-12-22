7875861cc17db48322057c4b3ce66e4235cd67d0
Pavel Minkov <pavel.minkov.it@gmail.com>
2021-12-22 21:53:38 +0200

    init

---
+++ RozetkaPageFactoryParallel/DataSource/Filters.cs
@@ -0,0 +1,16 @@
+﻿using RozetkaPageFactory.TestDataAccess;
+using System;
+using System.Collections.Generic;
+
+namespace RozetkaPageFactoryParallel.TestDataAccess
+{
+    [Serializable]
+    public class Filters
+    {
+        public List<Filter> FiltersList { get; set; }
+        public Filters()
+        {
+            FiltersList = new List<Filter>();
+        }
+    }
+}
---
+++ RozetkaPageFactoryParallel/Utils/ProperyReader.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+using System.Xml.Serialization;
+
+namespace RozetkaPageFactoryParallel.TestDataAccess
+{
+    class ProperyReader
+    {
+        private Properties properties;
+
+        public ProperyReader()
+        {
+            ReadPropertiesFromXML();
+        }
+
+        private void ReadPropertiesFromXML()
+        {
+            XmlSerializer xmlFormat = new XmlSerializer(typeof(Properties));
+            try
+            {
+                string path = Directory.GetCurrentDirectory();
+                path = path.Substring(0, path.Length - 17);
+                path = Path.Combine(path, @"DataSource\Properties.xml");
+                using (Stream fStream = File.OpenRead(path))
+                {
+                    properties = (Properties)xmlFormat.Deserialize(fStream);
+                }
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Can`t open properties file");
+            }
+        }
+        public string GetURL()
+        {
+            return properties.URL;
+        }
+    }
+}
